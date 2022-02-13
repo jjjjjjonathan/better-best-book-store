f0a5408a5ce32c26e79d9f59f3a43e84cc67338f
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-12 23:45:51 -0500

    Filter items table with search queries

--- routes/books.js
+++ routes/books.js
@@ -3,10 +3,28 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    // should turn queryString generator into a function
+    const search = req.query;
+    console.log(search);
+    const queryParams = [];
+    let queryString = `
+    SELECT items.*, photo_urls.photo_url FROM items JOIN photo_urls ON item_id = items.id`;
+    let whereConditions = [];
+    if (!!search.title) {
+      queryParams.push(`%${search.title}%`);
+      whereConditions.push(`items.title ILIKE $${queryParams.length}`);
+    }
+    if (!!search.genre) {
+      queryParams.push(`%${search.genre}%`);
+      whereConditions.push(`items.genre ILIKE $${queryParams.length}`);
+    }
+
+    if (queryParams.length > 0) {
+      queryString += ` WHERE ${whereConditions.join(' AND ')}`;
+    }
+    queryString += ";";
     return db
-      .query(
-        `SELECT * , photo_urls FROM items JOIN photo_urls ON item_id = items.id  GROUP BY items.id, photo_urls.id;`
-      )
+      .query(queryString, queryParams)
       .then((data) => {
         const items = data.rows;
         const templateVars = {
