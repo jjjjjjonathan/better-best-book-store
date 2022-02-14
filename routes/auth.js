0071ec88a7598b3082b59b8b8702effd4310af9a
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-13 19:07:53 -0500

    Implement cookie-session to login/out

--- routes/auth.js
+++ routes/auth.js
@@ -8,14 +8,23 @@ module.exports = db => {
 
     return db.query(`SELECT * FROM users;`)
       .then(data => {
-        const items = data.rows;
-        const templateVars = { items: items };
+        const users = data.rows;
+        const templateVars = { users: users };
         res.render('auth/login', templateVars);
       })
-      .catch();
+      .catch(error => {
+        console.log(error);
+      });
   });
   router.post('/', (req, res) => {
+    req.session['user_id'] = req.body['user_id'];
+    console.log('Logged in as: ', req.session['user_id']);
+    res.redirect('../');
+  });
+  router.post('/logout', (req, res) => {
+    console.log('Logging out as: ', req.session['user_id']);
     req.session = null;
+    res.redirect('../');
   });
   return router;
 };
