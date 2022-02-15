fb92511d222269fb69a47fbea7ae2bd9916366b4
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-14 21:05:16 -0500

    Pull user_id names from tables

--- routes/conversations.js
+++ routes/conversations.js
@@ -8,12 +8,14 @@ module.exports = db => {
     if (!userId) {
       res.render("index");
     } else {
-      return db.query(`SELECT conversations.id, count(messages.*) as message_count, conversations.user1_id as user1, conversations.user2_id as user2
+      return db.query(`SELECT conversations.id, count(messages.*) as message_count, users_1.name as name1, users_2.name as name2
       FROM messages
       JOIN conversations ON messages.conversation_id = conversations.id
+      JOIN users AS users_1 ON conversations.user1_id = users_1.id
+      JOIN users AS users_2 ON conversations.user2_id = users_2.id
       WHERE conversations.user1_id = ${userId}
       OR conversations.user2_id = ${userId}
-      GROUP BY conversations.id;`)
+      GROUP BY conversations.id, users_1.name, users_2.name;`)
         .then(data => {
           const messages = data.rows;
           const templateVars = { messages: messages };
