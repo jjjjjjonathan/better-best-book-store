56e9d4ce29c05be9c21ad17ecf834374a6cc0a03
JuliaaMuller <julialm.muller@gmail.com>
2022-02-15 11:19:25 -0500

    add  a new favorite page

---
+++ routes/my_favorites.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { searchQueryGenerator } = require('../public/scripts/helpers');
+const router = express.Router();
+
+
+// module.exports = (db) => {
+  router.get("/myfavorites", (req, res) => {
+let queryString = `SELECT * FROM favorites WHERE user_id = $1;`;
+let values = [req.session['user_id']];
+    return db.query(queryString, values)
+      .then((data) => {
+        console.log(data.rows)
+        // const items = data.rows;
+        // const templateVars = {
+        //   items: items,
+        // };
+        res.render("books/my_favorites");
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  // router.get('/search', (req, res) => {
+  //   res.render('books/search');
+  // });
+  // return router;
+// };
