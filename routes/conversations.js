2e8898194a1dc6800df88d6e01f12fdc65a593b1
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-15 16:24:38 -0500

    Add timestamp for latest message

--- routes/conversations.js
+++ routes/conversations.js
@@ -7,16 +7,18 @@ module.exports = db => {
     if (!userId) {
       res.render("index");
     } else {
-      return db.query(`SELECT conversations.id, count(messages.*) as message_count, users_1.username as name1, users_2.username as name2
+      return db.query(`SELECT conversations.id, count(messages.*) as message_count, users_1.username as name1, users_2.username as name2, conversations.last_message_time
       FROM messages
       JOIN conversations ON messages.conversation_id = conversations.id
       JOIN users AS users_1 ON conversations.user1_id = users_1.id
       JOIN users AS users_2 ON conversations.user2_id = users_2.id
       WHERE conversations.user1_id = $1
       OR conversations.user2_id = $1
-      GROUP BY conversations.id, users_1.username, users_2.username;`, [userId])
+      GROUP BY conversations.id, users_1.username, users_2.username
+      ORDER BY conversations.last_message_time;`, [userId])
         .then(data => {
           const messages = data.rows;
+          console.log(messages[0]);
           if (messages.length > 0) {
             const templateVars = { messages: messages };
             res.render('conversations/conversations', templateVars);
@@ -49,6 +51,9 @@ module.exports = db => {
     console.log(req.params, req.body);
     return db.query(`INSERT INTO messages (sender_id, conversation_id, message_body)
     VALUES ($1, $2, $3);`, [req.session['user_id'], parseFloat(req.params.id), req.body.message])
+      .then(() => {
+        db.query(`UPDATE conversations SET last_message_time = now() WHERE id = ${parseFloat(req.params.id)};`);
+      })
       .then(() => {
         res.redirect(`/conversations/${req.params.id}`);
       });
