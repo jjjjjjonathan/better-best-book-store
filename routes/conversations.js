f5547a88b878e2abee185003a11379ba8fc57cfb
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-15 15:08:39 -0500

    Create GET route for specific message threads

--- routes/conversations.js
+++ routes/conversations.js
@@ -7,14 +7,14 @@ module.exports = db => {
     if (!userId) {
       res.render("index");
     } else {
-      return db.query(`SELECT conversations.id, count(messages.*) as message_count, users_1.name as name1, users_2.name as name2
+      return db.query(`SELECT conversations.id, count(messages.*) as message_count, users_1.username as name1, users_2.username as name2
       FROM messages
       JOIN conversations ON messages.conversation_id = conversations.id
       JOIN users AS users_1 ON conversations.user1_id = users_1.id
       JOIN users AS users_2 ON conversations.user2_id = users_2.id
-      WHERE conversations.user1_id = ${userId}
-      OR conversations.user2_id = ${userId}
-      GROUP BY conversations.id, users_1.name, users_2.name;`)
+      WHERE conversations.user1_id = $1
+      OR conversations.user2_id = $1
+      GROUP BY conversations.id, users_1.username, users_2.username;`, [userId])
         .then(data => {
           const messages = data.rows;
           if (messages.length > 0) {
@@ -32,7 +32,17 @@ module.exports = db => {
   });
 
   router.get('/:id', (req, res) => {
-    console.log("yes");
+    return db.query(`SELECT messages.*, users.username as sender
+    FROM messages
+    JOIN conversations ON conversations.id = messages.conversation_id
+    JOIN users ON messages.sender_id = users.id
+    WHERE conversations.id = $1
+    ORDER BY sent_at;`, [req.params.id])
+      .then(data => {
+        const messageThread = data.rows;
+        const templateVars = { messageThread };
+        res.render('conversations/thread', templateVars);
+      });
   });
 
   return router;
