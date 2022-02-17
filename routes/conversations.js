35980902e288665ca1e3967670b0359557f2be09
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-17 14:40:50 -0500

    Fix missing item_id bug in messages

--- routes/conversations.js
+++ routes/conversations.js
@@ -26,7 +26,7 @@ module.exports = db => {
       GROUP BY conversations.id, items.title;`, [userId])
         .then(data => {
           const messages = data.rows;
-          console.log(messages)
+          console.log(messages);
           if (messages.length > 0) {
             for (const message of messages) {
               message['last_message_time'] = timeago.format(message['last_message_time']);
@@ -59,17 +59,19 @@ module.exports = db => {
   });
 
   router.get('/:id', (req, res) => {
-    return db.query(`SELECT messages.*, users.username as sender, users_1.username as user1, users_2.username as user2, users_1.id as id1, users_2.id as id2
+    return db.query(`SELECT messages.*, items.id AS item_id, users.username as sender, users_1.username as user1, users_2.username as user2, users_1.id as id1, users_2.id as id2
     FROM messages
     JOIN conversations ON conversations.id = messages.conversation_id
     JOIN users ON messages.sender_id = users.id
     JOIN users AS users_1 ON conversations.user1_id = users_1.id
     JOIN users AS users_2 ON conversations.user2_id = users_2.id
+    JOIN items ON items.id = messages.item_id
     WHERE conversations.id = $1
     ORDER BY sent_at DESC
     LIMIT 10;`, [req.params.id])
       .then(data => {
         const messageThread = data.rows.reverse();
+        console.log(messageThread[0]);
         const user = req.session.user_id;
         if (user === messageThread[0].id1 || user === messageThread[0].id2) {
           for (const message of messageThread) {
@@ -84,8 +86,8 @@ module.exports = db => {
   });
 
   router.post('/:id', (req, res) => {
-    return db.query(`INSERT INTO messages (sender_id, conversation_id, message_body)
-    VALUES ($1, $2, $3);`, [req.session['user_id'], parseFloat(req.params.id), req.body.message])
+    return db.query(`INSERT INTO messages (sender_id, conversation_id, message_body, item_id)
+    VALUES ($1, $2, $3, $4);`, [req.session['user_id'], parseFloat(req.params.id), req.body.message, parseFloat(req.body.item_id)])
       .then(() => {
         res.redirect(`/conversations/${req.params.id}`);
       });
