d4b2ec3f9f52e0c70011ea3386ff4df9036110d8
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-02-18 00:21:45 -0500

    Fix index route

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,36 +1,8 @@
 // Client facing scripts here
 const express = require("express");
 const router = express.Router();
-const { randomFeaturedItems } = require('./helpers');
-const timeago = require('timeago.js');
+
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    return db.query(`SELECT items.id AS item_id, items.title AS item_title, items.price AS item_price, users.username AS seller, items.created_at AS post_date, photo_urls.photo_url AS item_photo
-    FROM items
-    JOIN users ON users.id = items.owner_id
-    JOIN photo_urls ON photo_urls.item_id = items.id
-    WHERE users.super_seller = true
-    AND items.sold_status = false;`)
-      .then((data) => {
-        const items = data.rows;
-        for (const item of items) {
-          item['post_date'] = timeago.format(item['post_date']);
-        }
-        const i = randomFeaturedItems(items.length);
-        const templateVars = {
-          username: req.session["name"],
-          feature0: items[i[0]],
-          feature1: items[i[1]],
-          feature2: items[i[2]],
-          feature3: items[i[3]],
-          feature4: items[i[4]]
-        };
-        res.render("index", templateVars);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-  return router;
+
 };
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const express = require("express");
 const app = express();
 const cookieSession = require("cookie-session");
 const morgan = require("morgan");
+const timeago = require("timeago.js");
+const { randomFeaturedItems } = require('./public/scripts/helpers');
 
 // PG database client/connection setup
 const { Pool, Query } = require("pg");
@@ -68,7 +70,6 @@ app.use("/books", bookRoutes(db));
 app.use("/listings", listingsRoutes(db));
 app.use("/auth", authRoutes(db));
 app.use("/conversations", conversationRoutes(db));
-app.use("/", mainRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 
@@ -80,6 +81,34 @@ app.use("/", mainRoutes(db));
 //   const templateVars = { username: req.session.name };
 //   res.render("index", templateVars);
 // });
+
+app.get("/", (req, res) => {
+  return db.query(`SELECT items.id AS item_id, items.title AS item_title, items.price AS item_price, users.username AS seller, items.created_at AS post_date, photo_urls.photo_url AS item_photo
+  FROM items
+  JOIN users ON users.id = items.owner_id
+  JOIN photo_urls ON photo_urls.item_id = items.id
+  WHERE users.super_seller = true
+  AND items.sold_status = false;`)
+    .then((data) => {
+      const items = data.rows;
+      for (const item of items) {
+        item['post_date'] = timeago.format(item['post_date']);
+      }
+      const i = randomFeaturedItems(items.length);
+      const templateVars = {
+        username: req.session["name"],
+        feature0: items[i[0]],
+        feature1: items[i[1]],
+        feature2: items[i[2]],
+        feature3: items[i[3]],
+        feature4: items[i[4]]
+      };
+      res.render("index", templateVars);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
 app.get("/favorites", (req, res) => {
   let queryString = `SELECT favorites.id, favorites.item_id, favorites.user_id, photo_url as photo, items.title as title, items.price as price, items.owner_id as seller, items.genre as genre
